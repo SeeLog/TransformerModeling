8e1730a205c3acc2868840896c5ec8b348359041
SeeLog <seelog693@gmail.com>
2021-04-24 21:48:09 +0900

    サルベージしてきた

---
+++ transformer/models.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import copy
+
+from .layers import Encoder, Decoder, EncoderLayer, DecoderLayer
+from .sublayers import Embeddings, MultiHeadAttention, PositionalEncoding, PositionwiseFeedForward
+from .auto_encoder import AutoEncoder
+
+
+def make_model(src_vocab: int, tgt_vocab: int, num_layers: int = 6, d_model: int = 512, d_ff: int = 2048, head_num: int = 8, dropout: float = 0.1):
+    """
+    モデルを作成します．
+
+    :param src_vocab: SourceのVocabサイズ
+    :param tgt_vocab: TargetのVocabサイズ，-1でSourceとシェアする
+    :param num_layers: Transformerの層の数, default: 6
+    :param d_model: モデルの隠れ層のサイズ, default: 512
+    :param d_ff: FeedForwardの隠れ層のサイズ, default: 2048
+    :param head_num: MultiHeadAttentionの分割数, default: 8
+    :param dropout: Dropout率, default: 0.1
+
+    :return: 作成されたTransformerによるEncoderDecoderモデル
+    """
+    c = copy.deepcopy
+    attn = MultiHeadAttention(head_num, d_model)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    position = PositionalEncoding(d_model, dropout)
+
+    src_emb = Embeddings(d_model, src_vocab)
+    if tgt_vocab == -1:
+        tgt_emb = src_emb
+        tgt_vocab = src_vocab
+    else:
+        tgt_emb = Embeddings(d_model, tgt_emb)
+
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), num_layers),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), num_layers),
+        nn.Sequential(src_emb, c(position)),
+        nn.Sequential(tgt_emb, c(position)),
+        Generator(d_model, tgt_vocab))
+
+    # This was important from their code.
+    # Initialize parameters with Glorot / fan_avg.
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model
+    
+
+class Generator(nn.Module):
+    "Define standard linear + softmax generation step."
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab)
+
+    def forward(self, x):
+        return F.log_softmax(self.proj(x), dim=-1)
+
+
+class EncoderDecoder(nn.Module):
+    """
+    A standard Encoder-Decoder architecture. Base for this and many
+    other models.
+    """
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: Embeddings, tgt_embed: Embeddings, generator: Generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def forward(self, src: torch.tensor, tgt: torch.tensor, src_mask: torch.tensor, tgt_mask: torch.tensor) -> torch.tensor:
+        "Take in and process masked src and target sequences."
+        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+
+    def encode(self, src, src_mask) -> torch.tensor:
+        return self.encoder(self.src_embed(src), src_mask)
+
+    def decode(self, memory, src_mask, tgt, tgt_mask) -> torch.tensor:
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
---
+++ transformer/utils.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import copy
+import numpy as np
+import os
+import shutil
+
+
+class LabelSmoothing(nn.Module):
+    "Implement label smoothing."
+    def __init__(self, size, padding_idx, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(reduction="sum")
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = torch.zeros(x.size()).to(x.device)
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.padding_idx] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, true_dist)
+
+
+def attention(query: torch.tensor, key: torch.tensor, value: torch.tensor, mask=None, dropout=None) -> torch.tensor:
+    "Compute Simple 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+def subsequent_mask(size):
+    "Mask out subsequent positions."
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
+
+
+def greedy_decode(model, src: torch.tensor, src_mask: torch.tensor, max_len: torch.tensor, start_symbol: int):
+    """
+    Greedy Decoding
+
+    :param model: encode()とdecode()が実装されたモデル
+    :param src: Source Tensor
+    :param src_mask: Source Mask
+    :param max_len: Max Length
+    :param start_symbol: SOSシンボル
+
+    :return: Greedy DecodingされたTensor
+    """
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len - 1):
+        out = model.decode(memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data))
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    return ys
+
+
+class SimpleLossCompute:
+    "A simple loss compute and train function."
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1)) / norm
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.item() * norm
+
+
+def save_checkpoint(model: nn.Module, filepath: str, is_best: bool, epoch_i: int):
+    os.makedirs(filepath, exist_ok=True)
+    model_save_path = os.path.join(filepath, 'model_epoch_{}.pt'.format(epoch_i))
+    torch.save(model.state_dict(), model_save_path)
+    if is_best:
+        best_save_path = os.path.join(filepath, 'best_model.pt')
+        shutil.copyfile(model_save_path, best_save_path)
+
+
+def load_checkpoint(model, model_path, device, is_eval=True, is_file=False):
+    if is_file:
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+        return model.to(device)
+
+    if is_eval:
+        model.load_state_dict(torch.load(os.path.join(model_path, 'best_model.pt')))
+        model.eval()
+        return model.to(device=device)
+
+    model = model.load_state_dict(torch.load(os.path.join(model_path, 'last_model.pt')))
+    global_step = torch.load(os.path.join(model_path, 'global_step.pt'))
+    return model.to(device=device), global_step
